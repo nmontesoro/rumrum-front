62afb1bbc3914791f2af1a87c85f30533b5daf0f
Nazareno Montesoro <n.montesoro@gmail.com>
2023-10-04 19:16:20 -0300

    Refactor para incorporar mayor funcionalidad a futuro

--- code.ts
+++ code.ts
@@ -1,3 +1,9 @@
+type ControlMap = {
+  id: string;
+  events: Array<string>;
+  callbackFn: () => void;
+};
+
 type Controller = {
   container: HTMLElement;
   button: HTMLElement;
@@ -6,28 +12,64 @@ type Controller = {
 class AppStatus {
   #containers: NodeListOf<HTMLElement>;
 
-  #controlMap: Map<string, () => void> = new Map<string, () => void>([
-    ["autopilot-btn", () => this.#toggleContainer("autopilot")],
-    ["touchpad-btn", () => this.#toggleContainer("touchpad")],
-    ["word-btn", () => this.#toggleContainer("word")],
-    ["rotate-left-btn", () => console.log("left")],
-    ["rotate-right-btn", () => console.log("right")],
-    ["straight-btn", () => console.log("straight")],
-    ["back-btn", () => console.log("back")],
-    ["send-word-btn", () => console.log("word")],
-  ]);
+  #controlMap: Array<ControlMap> = [
+    {
+      id: "autopilot-btn",
+      events: ["click"],
+      callbackFn: () => this.#toggleContainer("autopilot"),
+    },
+    {
+      id: "touchpad-btn",
+      events: ["click"],
+      callbackFn: () => this.#toggleContainer("touchpad"),
+    },
+    {
+      id: "word-btn",
+      events: ["click"],
+      callbackFn: () => this.#toggleContainer("word"),
+    },
+    {
+      id: "rotate-left-btn",
+      events: ["click"],
+      callbackFn: () => console.log("left"),
+    },
+    {
+      id: "rotate-right-btn",
+      events: ["click"],
+      callbackFn: () => console.log("right"),
+    },
+    {
+      id: "straight-btn",
+      events: ["click"],
+      callbackFn: () => console.log("straight"),
+    },
+    {
+      id: "back-btn",
+      events: ["click"],
+      callbackFn: () => console.log("back"),
+    },
+    {
+      id: "send-word-btn",
+      events: ["click"],
+      callbackFn: () => console.log("word"),
+    },
+  ];
 
   constructor() {
     this.#containers = document.querySelectorAll('[id$="-container"]');
-    this.#controlMap.forEach((callbackFn, btnId) => {
-      let btn: HTMLElement | null = document.getElementById(btnId);
 
-      if (btn) {
-        btn.addEventListener("click", callbackFn);
+    this.#controlMap.forEach((controlMap) => {
+      let element: HTMLElement | null = document.getElementById(controlMap.id);
+
+      if (element) {
+        controlMap.events.forEach((event) => {
+          element?.addEventListener(event, controlMap.callbackFn);
+        });
       } else {
-        throw new Error(`No encontré el elemento con id ${btnId}`);
+        throw new Error(`No encontré el elemento con id ${controlMap.id}`);
       }
     });
+
     this.#toggleContainer("touchpad");
   }
 
